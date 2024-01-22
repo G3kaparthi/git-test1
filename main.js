73223767969d816a152a0c71df42b825e59645a9
gayathri <g3kaparthi@gmail.com>
2024-01-22 16:51:45 -0500

    my first

---
+++ main.js
@@ -0,0 +1,12 @@
+// main.js
+
+// Print a welcome message
+console.log("Hello, World!");
+
+// Calculate the sum of two numbers
+const num1 = 5;
+const num2 = 10;
+const sum = num1 + num2;
+
+// Print the sum
+console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
