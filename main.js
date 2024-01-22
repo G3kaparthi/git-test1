ffdf0a4c9e9bc84914d45cf38fb8aa156c3e7128
gayathri <g3kaparthi@gmail.com>
2024-01-22 18:11:48 -0500

    second

--- main.js
+++ main.js
@@ -4,9 +4,10 @@
 console.log("Hello, World!");
 
 // Calculate the sum of two numbers
-const num1 = 5;
+const num1 = 50;
 const num2 = 10;
 const sum = num1 + num2;
 
 // Print the sum
 console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+git branch devicePixelRatio
